346de5eb5b23f87aa2a1a9d03481b4d9d542106a
vishal929 <48373882+vishal929@users.noreply.github.com>
2023-06-01 20:27:43 -0400

    initial commit for neural network structure

---
+++ PiSystem/Models/model.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+from tensorflow import keras
+
+# using functional API to build our model
+# we are using a convolutional model, which should be channels first (for maximum support across platforms)
+# if we have a checkpoint, we load from the checkpoint, otherwise we build from scratch
+def buildModel(checkPointPath = None):
+    pass
