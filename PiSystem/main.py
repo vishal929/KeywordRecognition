fd9148885d322d9b0c0ad84a72f3fcfa4e0a6c18
vishal929 <48373882+vishal929@users.noreply.github.com>
2023-06-08 13:59:27 -0400

    main is good, testing with my voice and working great! I switched to the stream, callback paradigm for a little bit faster performance, it might make a big difference on the pi

--- PiSystem/main.py
+++ PiSystem/main.py
@@ -5,18 +5,21 @@
     3) if within the detection window we detect a keyword, we send a message to the corresponding microcontroller
     4) hopefully microcontroller receives the message properly and flips their designated switch
 '''
+import queue
+from time import time
 
-from Models.model import grab_tflite_model,build_model
+from Models.model import grab_tflite_model
 import os
 from constants import ROOT_DIR,INV_MAP,SAMPLING_RATE
 import tensorflow as tf
 import numpy as np
 import sounddevice as sd
-from Data.data_processing import load_audio_file,pad_window
 
 # function which sends a message to the specific microcontroller for this class to flip the switch
 def send_message(classname):
     pass
+
+
 
 if __name__ == '__main__':
 
@@ -24,8 +27,6 @@ if __name__ == '__main__':
 
     # grab our tflite model interpreter
     interpreter = grab_tflite_model(checkpoint_path)
-    #base_model = build_model(checkpoint_path)
-    #print(base_model.summary())
     interpreter.allocate_tensors()
     # Get input and output tensors.
     input_details = interpreter.get_input_details()
@@ -34,98 +35,101 @@ if __name__ == '__main__':
     # set constants needed during the inference loop
     arduino_flag = False
     arduino_win_count = 0
-    prob_threshold = 0.95
+    prob_threshold = 0.7
 
     # setup buffer for holding 3s of audio data
-    recording_samples = [np.zeros(SAMPLING_RATE),np.zeros(SAMPLING_RATE),np.zeros(SAMPLING_RATE)]
+    recording_samples = np.zeros(3*SAMPLING_RATE)
+    recording_queue = queue.Queue(maxsize=12)
 
     # check our sound devices
     print(sd.query_devices())
 
-    while True:
-        # inference loop
-        # record 1 second of audio and modify the numpy buffers
-        new_rec = sd.rec(frames=1*SAMPLING_RATE,samplerate=SAMPLING_RATE,channels=1,dtype='int16',blocking=False)
-        recording_samples[0] = recording_samples[1]
-        recording_samples[1] = recording_samples[2]
+    # setting up callback function for audio recording
+    def record_callback(indata, frames,time, status):
+        recording_queue.put(np.copy(indata[:,0]))
 
-        # waiting on the recording to finish and update buffer
-        sd.wait()
-        recording_samples[2] = np.squeeze(new_rec)
-        #print(recording_samples)
-        # need to convert to frequency space to input to model
-        tf_in = np.concatenate(recording_samples,axis=None)
-        #tf_in = load_audio_file(os.path.join(ROOT_DIR,"Data","Dataset","Test","Gym","Recording.m4a"))
-        #tf_in = pad_window(tf_in)
-        #tf_in = np.squeeze(sd.rec(frames=3*SAMPLING_RATE,
-                                  #samplerate=SAMPLING_RATE,channels=1,dtype='int16',blocking=True))
-        tf_in = tf.abs(
-            tf.signal.stft(
-                tf.convert_to_tensor(tf_in,dtype=tf.float32),
-                frame_length = 1024,
-                frame_step = 256,
-                pad_end=False
+    # setup continuous recording callback
+    stream = sd.InputStream(samplerate=SAMPLING_RATE,blocksize=1*SAMPLING_RATE,channels=1,callback=record_callback,dtype='int16')
+
+    # counting the time for windows
+    s = time()
+    with stream:
+        while True:
+            recording_sample = None
+            try:
+                # queue processing
+                recording_sample = recording_queue.get_nowait()
+                # if the queue is full, just dump it
+                if recording_queue.full():
+                    with recording_queue.mutex:
+                        recording_queue.queue.clear()
+            except queue.Empty as e:
+                # queue has no new elements we continue
+                continue
+            # update our buffer
+            # we first roll the old values, then update the end of the buffer with the new values
+            recording_samples = np.roll(recording_samples,-recording_sample.shape[0])
+            # updating the most recent part of the buffer with the new elements
+            recording_samples[-recording_sample.shape[0]:] = recording_sample
+
+            # need to convert to frequency space to input to model
+            tf_in = recording_samples
+
+            tf_in = tf.abs(
+                tf.signal.stft(
+                    tf.convert_to_tensor(tf_in, dtype=tf.float32),
+                    frame_length=1024,
+                    frame_step=256,
+                    pad_end=False
+                )
             )
-        )
-        # need to add channel dimension and batch dimension explicitly for compatibility with model
-        tf_in = tf.expand_dims(tf.expand_dims(tf_in,axis=0),axis=-1)
-        #print('input shape: ' + str(tf_in.shape))
-        #print(tf_in)
+            # need to add channel dimension and batch dimension explicitly for compatibility with model
+            tf_in = tf.expand_dims(tf.expand_dims(tf_in, axis=0), axis=-1)
 
-        # getting logits for this audio sample of 3 seconds
-        #print(input_details)
-        interpreter.set_tensor(input_details[0]['index'], tf_in)
-        interpreter.invoke()
-        logits = tf.squeeze(interpreter.get_tensor(output_details[0]['index']))
-        #real_logits = tf.squeeze(base_model.predict(tf_in))
-        #print(logits)
+            # getting logits for this audio sample of 3 seconds
+            interpreter.set_tensor(input_details[0]['index'], tf_in)
+            interpreter.invoke()
+            logits = tf.squeeze(interpreter.get_tensor(output_details[0]['index']))
 
-        # converting to probablities and seeing if we have some confidence in a class or not
-        # we have 5 classes, so a random-confidence is 0.2
-        # if we have a confidence in a class of 0.5 or greater, we will consider that as confirmed
+            # converting to probablities and seeing if we have some confidence in a class or not
+            # we have 5 classes, so a random-confidence is 0.2
+            # if we have a confidence in a class of 0.7 or greater, we will consider that as confirmed
 
-        preds = tf.nn.softmax(logits)
-        #real_preds = tf.nn.softmax(real_logits)
-        #print(preds)
-        #print(real_preds)
-        class_pred = tf.math.argmax(preds).numpy()
-        #print(class_pred)
-        prob = preds[class_pred]
-        detected_class = INV_MAP[class_pred]
-        if prob >= prob_threshold:
-            # resetting the recording array if we dont have silence
-            if detected_class != 'silence':
-                recording_samples[0] = np.zeros(SAMPLING_RATE)
-                recording_samples[1] = np.zeros(SAMPLING_RATE)
-                recording_samples[2] = np.zeros(SAMPLING_RATE)
+            preds = tf.nn.softmax(logits)
+            class_pred = tf.math.argmax(preds).numpy()
+            prob = preds[class_pred]
+            detected_class = INV_MAP[class_pred]
+            if prob >= prob_threshold:
+                if (not arduino_flag) and detected_class == 'arduino':
+                    # we have triggered the arduino flag, now we have to detect keywords and then send messages
+                    # this detection window lasts for 5s or until a valid class is detected
+                    print('arduino window initiated with probability: ' + str(prob))
+                    arduino_flag = True
+                    # resetting the time and our buffer
+                    recording_samples = np.zeros(3*SAMPLING_RATE)
+                    s = time()
+                    continue
+                elif arduino_flag and detected_class != 'arduino' and detected_class != 'silence':
+                    # we are in the detection window, and we have detected a keyword that is not silence
+                    # lets send a message to the corresponding microcontroller and reset the detection flag
+                    print('sending a message to class: ' + str(detected_class) + ' with probability: ' + str(prob))
+                    send_message(detected_class)
+                    arduino_flag = False
+                    arduino_win_count = 0
+                    print('arduino window stopped due to class given')
+                    # resetting the time and our buffer
+                    recording_samples = np.zeros(3 * SAMPLING_RATE)
+                    s = time()
+                    continue
 
-            if (not arduino_flag) and detected_class == 'arduino':
-                # we have triggered the arduino flag, now we have to detect keywords and then send messages
-                # this detection window lasts for 5s or until a valid class is detected
-                print('arduino window initiated with probability: ' + str(prob))
-                arduino_flag = True
-                continue
-            elif arduino_flag and detected_class != 'arduino' and detected_class != 'silence':
-                # we are in the detection window, and we have detected a keyword that is not silence
-                # lets send a message to the corresponding microcontroller and reset the detection flag
-                print('sending a message to class: ' + str(detected_class) + ' with probability: ' + str(prob))
-                send_message(detected_class)
-                arduino_flag = False
-                arduino_win_count=0
-                print('arduino window stopped')
-                continue
-
-        # 1s has passed
-        if arduino_flag:
-            arduino_win_count += 1
-            if arduino_win_count == 5:
-                # want to stop the window after 5 seconds has passed without a valid command
-                arduino_flag = False
-                arduino_win_count = 0
-                print('arduino window stopped')
-
-
-
-
-
-
+            # counting time
+            if arduino_flag:
+                new_time = time()
+                # updating elapsed time
+                arduino_win_count += new_time-s
+                s = new_time
+                if arduino_win_count >= 6:
+                    # want to stop the window after 6 seconds has passed without a valid command
+                    arduino_flag = False
+                    arduino_win_count = 0
+                    print('arduino window stopped due to time window')
