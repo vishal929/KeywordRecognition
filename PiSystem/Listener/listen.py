bf3ef1492139434f77902459a98d9a1942aeb060
vishal929 <48373882+vishal929@users.noreply.github.com>
2023-06-20 03:09:39 -0400

    try the serial bluetooth service on the rpi

--- PiSystem/Listener/listen.py
+++ PiSystem/Listener/listen.py
@@ -2,7 +2,7 @@
 # this code is based on the rfcomm server example code in pybluez git repo
 
 # below import is for serial option
-#from bluetooth import BluetoothSocket,PORT_ANY,RFCOMM,SERIAL_PORT_PROFILE,SERIAL_PORT_CLASS,advertise_service
+from bluetooth import BluetoothSocket,PORT_ANY,RFCOMM,SERIAL_PORT_PROFILE,SERIAL_PORT_CLASS,advertise_service
 import asyncio
 from threading import Thread
 
@@ -20,6 +20,7 @@ from PiSystem.Messaging.message import send_message
 desc1_uuid = "2901"
 desc2_uuid = "2902"
 # service for listening to phone messages as a peripheral for BLE
+'''
 class ListenerService(Service):
     # we pass in our message handler
     def __init__(self):
@@ -31,12 +32,6 @@ class ListenerService(Service):
     @characteristic(tx_uuid,CharFlags.NOTIFY)
     def read_message(self,options):
         return
-
-    ''' 
-    @descriptor(desc2_uuid,DescFlags.READ)
-    def read_message_desc2(self,options):
-        return
-    '''
 
     @descriptor(desc1_uuid,read_message,DescFlags.READ | DescFlags.WRITE)
     def read_message_desc1(self,value,options):
@@ -90,6 +85,53 @@ class ListenThread(Process):
 
     def run(self):
        asyncio.run(self.start_ble_logic())
+'''
+
+class BluetoothListener(Process):
+    def __init__(self):
+        super().__init__()
+        self.server_sock = BluetoothSocket(RFCOMM)
+        self.server_sock.bind(("", PORT_ANY))
+        self.server_sock.listen(1)
+
+        self.port = self.server_sock.getsockname()[1]
+
+        serial_uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+        # rx_uuid = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
+        # tx_uuid = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+        # uuid = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
+    def run(self) -> None:
+        advertise_service(self.server_sock, "SampleServer", service_id=uuid,
+                          service_classes=[uuid, SERIAL_PORT_CLASS],
+                          # profiles=[SERIAL_PORT_PROFILE],
+                          # protocols=[OBEX_UUID]
+                          )
+
+        print("Waiting for connection on RFCOMM channel", self.port)
+
+        client_sock, client_info = self.server_sock.accept()
+        print("Accepted connection from", client_info)
+
+        try:
+            while True:
+                data = client_sock.recv(1024)
+                if not data:
+                    break
+                print("Received", data)
+        except OSError:
+            pass
+
+        print("Disconnected.")
+
+        client_sock.close()
+        self.server_sock.close()
+        print("All done.")
+
+if __name__=="__main__":
+    proc = BluetoothListener()
+    proc.start()
+    proc.join()
+
 
 '''
 IN CASE YOU WANT TO USE SERIAL OR YOUR DEVICE DOES NOT SUPPORT LOW ENERGY BLUETOOTH
